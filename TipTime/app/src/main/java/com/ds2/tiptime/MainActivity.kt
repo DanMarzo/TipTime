cfc56969df840e4d6eafb418102108f92eba68a1
DanMarzo <marzogildan@gmail.com>
2022-09-06 20:11:50 -0300

    Controle e manipulação de textos

--- TipTime/app/src/main/java/com/ds2/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/ds2/tiptime/MainActivity.kt
@@ -2,10 +2,20 @@ package com.ds2.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ds2.tiptime.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val costOfService = binding.inputEntradaGorjeta
+        costOfService.text = "Hi, ?".toString()
+
+        binding.inputEntradaGorjeta.text = "vixe, mudou!".toString()
     }
 }
