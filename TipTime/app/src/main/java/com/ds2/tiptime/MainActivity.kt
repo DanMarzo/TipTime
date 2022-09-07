d58f548536701497ff85c8289d17a13c2c995682
DanMarzo <marzogildan@gmail.com>
2022-09-06 22:01:15 -0300

    create fun calculateTip

--- TipTime/app/src/main/java/com/ds2/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/ds2/tiptime/MainActivity.kt
@@ -10,18 +10,45 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.botaoCalcular.setOnClickListener{CalculateTip()}
+        setContentView(R.layout.activity_main)
+        binding.botaoCalcular.setOnClickListener { calculateTip() }
 
-        /*val costOfService = binding.inputEntradaGorjeta
-        costOfService.text = "Hi, ?".toString()
-
-        binding.inputEntradaGorjeta.text = "vixe, mudou!".toString()*/
+//        val costOfService = binding.inputEntradaGorjeta
+//        costOfService.text = "Hi, ?".toString()
+//
+//        binding.inputEntradaGorjeta.text = "vixe, mudou!".toString()
     }
 
-    fun CalculateTip()
+    fun calculateTip()
     {
+        val stringInEdit = binding.inputEntradaGorjeta.toString()
+        val const = stringInEdit.toDouble()
 
+        val selectId = binding.avaliarOpcoes.checkedRadioButtonId
+
+        val tipPercentage = when (selectId)
+        {
+            R.id.opcaoIncrivel -> 0.20
+            R.id.opcaoBom -> 0.18
+            else -> 0.15
+        }
+
+        /*if (radioButon1)
+        {
+            val porcentagem = 0.2
+        }
+        if (radioButon1)
+        {
+            val porcentagem = 0.18
+        }
+        if (radioButon1)
+        {
+            val porcentagem = 0.15
+        }
+
+        var resultado = valorDaTextBox * porcentagem
+
+        var tipTotal = binding.tipTotal.*/
     }
 }
